6245e0e70f41e5ac7941644b5d3174ca6f305667
Pascal Rettig <pascal@cykod.com>
2011-01-25 15:58:09 -0500

    Modified search to use ContentNodeValues rather than actual nodes

--- app/controllers/content_advanced/page_renderer.rb
+++ app/controllers/content_advanced/page_renderer.rb
@@ -7,35 +7,45 @@ class ContentAdvanced::PageRenderer < ParagraphRenderer
   paragraph :most_viewed_content
 
   def search_trends
-    @options = paragraph_options :search_trends
-    from = @options.days_ago.days.ago.at_midnight
-    duration = (@options.days_ago + 1).days
-    groups = ContentUserSearch.searches @options.days_ago.days.ago.at_midnight, duration, 1
-    group = groups[0]
-    target_ids = group.domain_log_stats.find(:all, :order => 'hits DESC', :limit => @options.limit).collect(&:target_id)
-    @keywords = ContentSearchKeyword.find :all, :conditions => {:id => target_ids, :approved => true, :has_results => true}
-    render_paragraph :feature => :content_advanced_page_search_trends
+
+    results = renderer_cache(nil,nil,:expires => 360) do |cache|
+      @options = paragraph_options :search_trends
+      from = @options.days_ago.days.ago.at_midnight
+      duration = (@options.days_ago + 1).days
+      groups = ContentUserSearch.searches @options.days_ago.days.ago.at_midnight, duration, 1
+      group = groups[0]
+      target_ids = group.domain_log_stats.find(:all, :order => 'hits DESC', :limit => @options.limit).collect(&:target_id)
+      @keywords = ContentSearchKeyword.find :all, :conditions => {:id => target_ids, :approved => true, :has_results => true}
+      cache[:output] = content_advanced_page_search_trends_feature
+    end
+
+    render_paragraph :text => results.output
   end
 
   def most_viewed_content
-    @options = paragraph_options :most_viewed_content
-    from = @options.days_ago.days.ago.at_midnight
-    duration = (@options.days_ago + 1).days
 
-    groups = ContentNode.traffic from, duration, 1
-    group = groups[0]
-    target_ids = group.domain_log_stats.find(:all, :order => 'hits DESC', :limit => @options.limit).collect(&:target_id)
-    @most_viewed = ContentNode.all :conditions => {:id => target_ids}
+    results = renderer_cache(nil,nil,:expires => 360) do |cache|
+      @options = paragraph_options :most_viewed_content
+      from = @options.days_ago.days.ago.at_midnight
+      duration = (@options.days_ago + 1).days
 
-    groups = Comment.commented from, duration, 1
-    group = groups[0]
-    target_ids = group.domain_log_stats.collect(&:target_id)
-    @most_commented = ContentNode.all :conditions => {:id => target_ids}
+      groups = ContentNode.traffic from, duration, 1
+      group = groups[0]
+      target_ids = group.domain_log_stats.find(:all, :order => 'hits DESC', :limit => @options.limit).collect(&:target_id)
+      @most_viewed = ContentNodeValue.all :conditions => {:content_node_id => target_ids}
 
-    groups = Share::EmailFriend.emailed from, duration, 1
-    group = groups[0]
-    target_ids = group.domain_log_stats.collect(&:target_id)
-    @most_shared = ContentNode.all :conditions => {:id => target_ids}
-    render_paragraph :feature => :content_advanced_page_most_viewed_content
+      groups = Comment.commented from, duration, 1
+      group = groups[0]
+      target_ids = group.domain_log_stats.find(:all,:order => 'hits DESC', :limit => @options.limit).collect(&:target_id)
+      @most_commented = ContentNodeValue.all :conditions => {:content_node_id => target_ids}
+
+      groups = Share::EmailFriend.emailed from, duration, 1
+      group = groups[0]
+      target_ids = group.domain_log_stats.find(:all,:order => 'hits DESC', :limit => @options.limit).collect(&:target_id)
+
+      @most_shared = ContentNodeValue.all :conditions => {:content_node_id => target_ids}
+      cache[:output] = content_advanced_page_most_viewed_content_feature
+    end
+    render_paragraph :text => results.output
   end
 end
